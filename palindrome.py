f1cc0641b5c74b2cca8d8177782b79e0e1a43405
gouri <smchikkeri5757@gmail.com>
2025-11-27 14:29:57 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,14 @@
 import sys
-if len(sys.argv) != 2:
-    print("ERROR: Please provide exactly 1 string as a parameter.")
-    print("Usage: python3 palindrome.py <STRING>")
-    sys.exit(1)
-input_str = sys.argv[1]
-print("\n=== INPUT RECEIVED FROM JENKINS PARAMETERS ===")
-print("String:", input_str)
-normalized = input_str.replace(" ", "").lower()
-is_palindrome = normalized == normalized[::-1]
-print("\n===== RESULT =====")
-if is_palindrome:
-    print(f"'{input_str}' is a Palindrome ✅")
+
+if len(sys.argv)==2:
+  script_name = sys.argv[0]
+  name = sys.argv[1]
+
 else:
-    print(f"'{input_str}' is NOT a Palindrome ❌")
+  name = "gadag"
+
+if name == name[::-1]:
+  print(f"{name} is a palindrome")
+
+else:
+  print(f"{name} is not a palindrome")
